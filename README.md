4810e618b525c89b2afe41ade25be7db0e01e0a6
tarek abdelhak <tarekroja123@gmail.com>
2022-01-23 23:51:43 +0200

    initial

---
+++ README.md
@@ -0,0 +1,31 @@
+![landing page preview](/assets/landing_page.png)
+
+# Landing Page
+
+## Project Description
+
+Landing Page is a Dynamic single page web application, it's created to receive sections of data and adapt to it dynamically .  
+the navigation menu is updated automatically to respond to the coming secitons where via links added the navigation menu automatically .
+
+## installation
+
+1. Download the Zip file
+2. unzip the downloaded file
+3. display index.html in your browser
+
+## usage
+
+this web application is used to adapt to any coming data by automatically updating the navigation menue to link to the added sections .
+
+## Development
+
+the navigation menu is created and updated in the **app.js file** .  
+clicking on the **plus icon** adds new sections in the page and you will notice that new links are created and added to the navigation menu .  
+clicking on the **arrow icon** will take you to the top of the page .  
+also you can navigate **smoothly** to any section by clicking on the section link in the navigation menue .  
+the current viewed section is **highlighted with animation and darker background-color with highlighted link in the navigation menue** .
+
+## references
+
+information on how the inViewPort function is detecting the current viewed section can be found in this article
+[isSectionInViewPort](https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/)
---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Define Global Variables
+
+// getting all sections in the document
+let sections = Array.from(document.querySelectorAll("section"));
+// button to add sections dynamically
+let addSectionButton = document.createElement("button");
+// getting the wrapper of list items which is <ul></ul>
+let navList = document.querySelector("#navbar__list");
+// End Global Variables
+
+// Start Helper Functions
+
+// setup a function to get the current active section
+function getCurrentActive(sections) {
+  let active = null;
+  sections.forEach((section) => {
+    if (section.classList.contains("active")) {
+      active = section;
+    }
+  });
+  return active;
+}
+
+// setup a function to create a new section
+function createSection() {
+  let section = document.createElement("section");
+  section.setAttribute("id", `section+${sections.length + 1}`);
+  section.setAttribute("data-nav", `section ${sections.length + 1}`);
+  section.innerHTML = `<div class='landing__container'><h2>${section.getAttribute(
+    "data-nav"
+  )}</h2><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod. </p> <p> Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non. </p> </div>`;
+  document.querySelector("main").appendChild(section);
+  sections.push(section);
+  return section;
+}
+
+// setup a function to create a list item anchor
+function createListItem(id, data) {
+  let item = document.createElement("li");
+  item.innerHTML = `<a href="#${id}">${data}</a>`;
+  return item;
+}
+
+// setup a function to determine the current viewport section
+function inViewPort(section) {
+  const rectangle = section.getBoundingClientRect();
+  return (
+    rectangle.top >= 0 &&
+    rectangle.left >= 0 &&
+    rectangle.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rectangle.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// End Helper Functions
+
+// Begin Main Functions
+
+// build the nav
+function buildNav(sections) {
+  sections.forEach((section) => {
+    // create the list item
+    let listItem = createListItem(section.id, section.getAttribute("data-nav"));
+    // append the list item to the wrapper <ul></ul>
+    navList.appendChild(listItem);
+  });
+}
+buildNav(sections);
+// End Main Functions
+
+// Begin Events
+
+// Add class 'active' to section when near top of viewport
+window.addEventListener("scroll", () => {
+  // if the section is in the viewport, then it's active
+  sections.forEach((section) => {
+    if (inViewPort(section)) {
+      // remove the active from the current class
+      getCurrentActive(sections).classList.remove("active");
+      // add the active to the one that appeared in its full height
+      section.classList.add("active");
+      let anchors = document.querySelectorAll("a");
+      anchors.forEach((anchor) => {
+        anchor.classList.remove("active");
+        if (anchor.href.includes(section.id)) {
+          anchor.classList.add("active");
+          console.log(section);
+        }
+      });
+    }
+  });
+});
+
+// making the button creates new sections
+document.querySelector(".btn").addEventListener("click", () => {
+  let section = createSection();
+  let item = createListItem(section.id, section.getAttribute("data-nav"));
+  navList.appendChild(item);
+});
+
+// Scroll to anchor ID using scrollTO event
+
+document.querySelector("#up").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+  });
+});
